1238005dd94ce2eadb3082f6f4aebbf6944f26b6
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-02-13 11:59:48 +0700

    setup database connection

---
+++ database/postgres.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"fmt"
+	"github.com/mfitrahrmd/BTPN_Syariah-Image_Uploader/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	Database string
+}
+
+func RunDatabase(config Config, runMigration bool) *gorm.DB {
+	conn, err := gorm.Open(postgres.Open(fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", config.User, config.Password, config.Host, config.Port, config.Database)))
+	if err != nil {
+		panic(fmt.Sprintf("err connecting to database : %v\n", err))
+	}
+
+	if runMigration {
+		err = conn.AutoMigrate(&models.User{}, &models.Photo{})
+		if err != nil {
+			panic(fmt.Sprintf("err running database migration : %v\n", err))
+		}
+	}
+
+	return conn
+}
