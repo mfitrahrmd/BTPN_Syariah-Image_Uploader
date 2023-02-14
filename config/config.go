5ed06e2d7e7e16b748a06a860c7714b2e4232df7
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-02-14 09:35:49 +0700

    add application environment configuration

---
+++ config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AppEnv string
+	App    struct {
+		Host string
+		Port int
+	}
+	Database struct {
+		Host     string
+		Port     int
+		User     string
+		Password string
+		Database string
+	}
+	Jwt struct {
+		SecretKey                string
+		TokenExpirationInMinutes int
+	}
+}
+
+func LoadConfig(configFilePath string) (Config, error) {
+	pflag.String("appenv", "dev", "application environment : dev | prod")
+
+	pflag.Parse()
+	err := viper.BindPFlags(pflag.CommandLine)
+	if err != nil {
+		return Config{}, fmt.Errorf("err bind p flags : %w", err)
+	}
+
+	configName := fmt.Sprintf("config.%s", viper.GetString("appenv"))
+
+	fmt.Println(configName)
+
+	viper.SetConfigName(configName)
+	viper.AddConfigPath(configFilePath)
+	err = viper.ReadInConfig()
+	if err != nil {
+		return Config{}, fmt.Errorf("err read in config : %w", err)
+	}
+
+	var config Config
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return Config{}, fmt.Errorf("err unmarshaling config : %w", err)
+	}
+
+	return config, err
+}
