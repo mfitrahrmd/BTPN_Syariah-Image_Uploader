925f008497b4f796772f59e49f554447768b430b
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-02-15 12:41:26 +0700

    edit some db connection params

--- database/postgres.go
+++ database/postgres.go
@@ -3,30 +3,36 @@ package database
 import (
 	"fmt"
 	"github.com/mfitrahrmd/BTPN_Syariah-Image_Uploader/models"
+	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 )
 
 type Config struct {
 	User     string
 	Password string
 	Host     string
-	Port     int
+	Port     string
 	Database string
 }
 
-func RunDatabase(config Config, runMigration bool) *gorm.DB {
-	conn, err := gorm.Open(postgres.Open(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", config.User, config.Password, config.Host, config.Port, config.Database)))
+func StartDatabase(config Config, runMigration bool) (*gorm.DB, error) {
+	conn, err := gorm.Open(postgres.Open(fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", config.User, config.Password, config.Host, config.Port, config.Database)))
 	if err != nil {
-		panic(fmt.Sprintf("err connecting to database : %v\n", err))
+		return nil, fmt.Errorf("err connecting to database : %w", err)
 	}
+
+	logrus.Infoln("[~] database connected")
 
 	if runMigration {
 		err = conn.AutoMigrate(&models.User{}, &models.Photo{})
 		if err != nil {
-			panic(fmt.Sprintf("err running database migration : %v\n", err))
+			return nil, fmt.Errorf("err running database migration : %w", err)
 		}
+
+		log.Println("[~] database migration successfully")
 	}
 
-	return conn
+	return conn, nil
 }
