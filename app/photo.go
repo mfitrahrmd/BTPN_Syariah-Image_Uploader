4f70d19b831fc3ee625401313217fc0cae3786da
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-02-16 12:31:50 +0700

    define photo DTO

---
+++ app/photo.go
@@ -0,0 +1,39 @@
+package app
+
+type Photo struct {
+	ID       uint   `json:"id,omitempty"`
+	Title    string `json:"title,omitempty"`
+	Caption  string `json:"caption,omitempty"`
+	PhotoUrl string `json:"photoUrl,omitempty"`
+	UserID   uint   `json:"userID,omitempty"`
+}
+
+type InsertPhotoRequest struct {
+	Title    string `json:"title" binding:"required"`
+	Caption  string `json:"caption" binding:"required"`
+	PhotoUrl string `json:"photoUrl" binding:"required,url"`
+}
+
+type InsertPhotoResponse struct {
+	ID       uint   `json:"id,omitempty"`
+	Title    string `json:"title,omitempty" binding:"required"`
+	Caption  string `json:"caption,omitempty" binding:"required"`
+	PhotoUrl string `json:"photoUrl,omitempty" binding:"required,url"`
+}
+
+type FindAllPhotosResponse struct {
+	Photos []Photo `json:"photos"`
+}
+
+type UpdatePhotoRequest struct {
+	Title    string `json:"title,omitempty" binding:"required"`
+	Caption  string `json:"caption,omitempty" binding:"required"`
+	PhotoUrl string `json:"photoUrl,omitempty" binding:"required,url"`
+}
+
+type UpdatePhotoResponse struct {
+	ID       uint   `json:"id,omitempty"`
+	Title    string `json:"title,omitempty"`
+	Caption  string `json:"caption,omitempty"`
+	PhotoUrl string `json:"photoUrl,omitempty"`
+}
