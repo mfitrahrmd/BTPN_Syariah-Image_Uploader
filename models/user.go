07289ceb82cc08ba115949a765584e120a9ebd71
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-02-16 12:21:19 +0700

    add json tag to user model

--- models/user.go
+++ models/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string  `gorm:"type:string;size:16;not null;unique"`
-	Email    string  `gorm:"type:string;size:50;not null;unique;index"`
-	Password string  `gorm:"type:string;not null"`
-	Photos   []Photo `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Username string  `json:"username,omitempty" gorm:"type:string;size:16;not null;unique"`
+	Email    string  `json:"email,omitempty" gorm:"type:string;size:50;not null;unique;index"`
+	Password string  `json:"password,omitempty" gorm:"type:string;not null"`
+	Photos   []Photo `json:"photos,omitempty" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
