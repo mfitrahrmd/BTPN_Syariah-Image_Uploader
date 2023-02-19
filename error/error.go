7d9710b6c6b3ae7923b49d5cc7556141cd7ac077
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-02-19 20:20:26 +0700

    add error details

--- error/error.go
+++ error/error.go
@@ -3,12 +3,20 @@ package controllerError
 type ControllerError struct {
 	StatusCode int
 	Message    string
+	Details    any
 }
 
-func New(statusCode int, message string) ControllerError {
+func New(statusCode int, message string, details ...any) ControllerError {
 	return ControllerError{
 		StatusCode: statusCode,
 		Message:    message,
+		Details: func() any {
+			if len(details) > 0 {
+				return details[0]
+			}
+
+			return nil
+		}(),
 	}
 }
 
